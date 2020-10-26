ba8bfd91433da9fa97eb7c7ab8d6ae19be1cf5c1
Adil <adilAkhmetovDev@gmail.com>
2020-10-26 16:40:57 +0600

    check github workflows

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application } from 'express';
+import add from './add';
 
 const app: Application = express();
 
 app.get('/', (req, res) => {
-  res.send('Aru krasavica (ne)');
+  res.send('Aru krasavica');
 });
+
+console.log(add(5, 10));
 
 app.listen(5000, () => console.log('Server is running'));
